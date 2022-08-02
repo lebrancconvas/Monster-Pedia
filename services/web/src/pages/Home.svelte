0262c09cddbf641efa9b9093c207d681df61aa54
lebrancconvas <lebrancconvas@gmail.com>
2022-08-02 19:51:31 +0700

    ✨ feat: update web

---
+++ services/web/src/pages/Home.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+</script>
+
+<main>
+	<header>
+		<div id="heading">
+			<h1>Monsterpedia</h1>
+		</div>
+		<div id="bio">
+			<h2>All Information that you want to know for Monster Arena</h2> 
+		</div>
+	</header>
+	<section>
+		<div id="wiki-button">
+			<div id="wiki-button-content">
+				Wiki
+			</div>
+		</div>
+	</section>
+</main>
+
+<style>
+  main {
+		font-family: 'Itim', cursive;
+		text-align: center;
+		/* display: inline-block; */
+		/* vertical-align: middle; */
+	}
+
+	#wiki-button {
+		border: 2px solid black;
+		width: 10%;
+		padding: 5px;
+		border-radius: 10px;
+		margin: 10px auto;
+		background-color: orange;
+		box-shadow: 1px 1px 1px 1px black;
+		cursor: pointer;
+	}
+
+	#wiki-button:active {
+		transform: scale(0.98); 
+	}
+
+	#wiki-button-content {
+		font-size: 30px;
+	}
+</style>
